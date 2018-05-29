eede16c56c214de99a55e1d1de684d617aded354
Travis_S <dragomir1998@abv.bg>
2018-05-29 21:50:03 +0300

    Changes

--- MovieProject/Controllers/HomeController.cs
+++ MovieProject/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MovieProject.Helpers;
+using MovieProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -26,5 +28,77 @@ namespace MovieProject.Controllers
 
             return View();
         }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public ActionResult LoginPost(LoginFormModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+         //<<<<<<<<<<<<
+        public class Newcomer
+        {
+            public int ID { get; set; }
+            public string Username { get; set; }
+            public bool IsAdministrator { get; set; }
+        }
+        public class NewcomerRepository
+        {
+            public Newcomer GetUserByNameAndPassword(string username, string password)
+            {
+                return new Newcomer()
+                {
+                    ID = 2,
+                    Username = username,
+                    IsAdministrator = true
+                };
+            }
+        }
+        [HttpPost]
+        [ActionName("Login")]
+        public ActionResult LoginPostntnt(LoginFormModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                #region mockup classes
+                // here we have to check if the username exists in the database
+                NewcomerRepository newcomerRepository = new NewcomerRepository();
+                Newcomer dbUser = newcomerRepository.GetUserByNameAndPassword(viewModel.Username, viewModel.Password);
+
+                bool isUserExists = dbUser != null;
+                if (isUserExists)
+                {
+                    UserLoginProcess.Current.SetCurrentUser(dbUser.ID, dbUser.Username, dbUser.IsAdministrator);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username and/or password");
+                }
+                #endregion
+            }
+            // if we are here, this means there is some validation error and we have to show the login screen again
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            UserLoginProcess.Current.Logout();
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
---
+++ MovieProject/Helpers/UserLoginProcess.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieProject.Helpers
+{
+    public class UserLoginProcess
+    {
+        public int UserID { get; private set; }
+        public string Username { get; private set; }
+        public bool IsAuthenticated { get; private set; }
+        public bool IsAdministrator { get; private set; }
+
+        private UserLoginProcess()
+        {   // IsAuthenticated = false;  
+        }
+
+        public static UserLoginProcess Current
+        {
+            get
+            {
+                UserLoginProcess UserLoginProcess = (UserLoginProcess)HttpContext.Current.Session["LoginUser"];
+                if (UserLoginProcess == null)
+                {
+                    UserLoginProcess = new UserLoginProcess();
+                    HttpContext.Current.Session["LoginUser"] = UserLoginProcess;
+                }
+                return UserLoginProcess;
+            }
+        }
+
+        public void SetCurrentUser(int userID, string username, bool isAdministrator)
+        {
+            this.IsAuthenticated = true;
+            this.IsAdministrator = isAdministrator;
+            this.UserID = userID;
+            this.Username = username;
+        }
+
+        public void Logout()
+        {
+            this.IsAuthenticated = false;
+            this.IsAdministrator = false;
+            this.UserID = 0;
+            this.Username = string.Empty;
+        }
+
+    }
+}
